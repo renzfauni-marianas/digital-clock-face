e7713a02196e70f6237a763a02945cb50e0da1db
renzfauni-marianas <111693650+renzfauni-marianas@users.noreply.github.com>
2022-11-29 18:03:54 +1000

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,29 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setClockFace(){
+    //now const variable is not global its local to the method
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds /60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes /60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours /60) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    var time = padTwo(hours) + ":" + padTwo(minutes) + ":" + padTwo(seconds);
+    const digitalClockFace = document.querySelector('.time');
+    digitalClockFace.innerHTML = time;
+}
+
+function padTwo(number){
+    return(number < 10 ? 'O' : '') + number;
+}
+//1000 milliseconds is one second
+setInterval(setClockFace,1000);
